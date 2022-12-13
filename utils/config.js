9a9023c6fe8e22b52e3383e71ef9c805732015c1
anewlens <36173414+anewlens@users.noreply.github.com>
2022-12-12 22:34:24 -0500

    change hosting

--- utils/config.js
+++ utils/config.js
@@ -1,10 +1,10 @@
-require('dotenv').config()
+require("dotenv").config()
 
 let PORT = process.env.PORT
 
-let MONGO_URI = process.env.MONGO_URI
+let MONGO_URI = process.env.DATABASE
 
 module.exports = {
-    MONGO_URI,
-    PORT
+  MONGO_URI,
+  PORT,
 }
